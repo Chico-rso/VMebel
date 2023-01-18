7eea0a79e45641921ab6bf31f84fa46b8aea6d6e
Amiran <chicobes1@gmail.com>
2023-01-18 21:32:54 +0300

    separated styles from components on the main page

--- src/components/mainMenuList/MainMenuListItem.vue
+++ src/components/mainMenuList/MainMenuListItem.vue
@@ -17,16 +17,5 @@ const props = defineProps({
 </script>
 
 <style lang="scss">
-.main-menu-list-item
-{
-	margin-bottom: 10px;
-	&:last-child{margin-bottom: 0;}
-	a
-	{
-		font-weight: $regular;
-		font-size: 16px;
-		line-height: 19px;
-		color: #6C6C6C;
-	}
-}
+@import "mainMenuListItem.scss";
 </style>
