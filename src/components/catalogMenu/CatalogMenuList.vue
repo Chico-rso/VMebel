00de8dc2ae575e46a34938a21172fb2fed19d214
Amiran <chicobes1@gmail.com>
2023-01-25 11:50:38 +0300

    created catalog menu list openning logic

--- src/components/catalogMenu/CatalogMenuList.vue
+++ src/components/catalogMenu/CatalogMenuList.vue
@@ -1,58 +1,81 @@
 <template>
 	<div>
-		<div v-for="item in catalogArr" :key="item.id" @mouseover="openChildren(item)" @click="closeChildren(item)">
+		<div v-for="item in catalogArr" :key="item.id" @mouseover="cycleChildren(item)"
+		     @mouseleave="closeChildren(item)">
 			{{ item.name }}
-			<div v-if="item.isOpen" v-for="child in item.children" :key="child.id" @mouseover="openChildren(child)"
-			     @click="closeChildren(child)">
+			<div v-if="item.isOpen" v-for="child in item.children" :key="child.id" @mouseover="cycleChildren(child)"
+			     @mouseleave="closeChildren(child)">
 				{{ child.name }}
 				<div v-if="child.isOpen" v-for="grandchild in child.children" :key="grandchild.id"
-				     @click="closeChildren(grandchild)">
+				     @mouseover="cycleChildren(grandchild)" @mouseleave="closeChildren(grandchild)">
 					{{ grandchild.name }}
+					<div v-if="grandchild.isOpen" v-for="greatGrandchild in grandchild.children"
+					     :key="greatGrandchild.id">
+						{{ greatGrandchild.name }}
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
-<script>
-export default {
-	data()
+<script setup>
+import {ref} from "vue";
+
+const catalogArr = ref([
 	{
-		return {
-			catalogArr: [
+		id: "1",
+		name: "Мягкая мебель",
+		isOpen: false,
+		children: [
+			{
+				id: "1.1",
+				name: "Диваны",
+				isOpen: false,
+				children: [
+					{
+						id: "1.1.1",
+						name: "Детские",
+						isOpen: false,
+						children: [
+							{
+								id: "1.1.1.1",
+								name: "Надувные",
+								isOpen: false,
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+]);
+
+function cycleChildren(item)
+{
+	if (item.children)
+	{
+		item.isOpen = true;
+	}
+}
+
+function closeChildren(item)
+{
+	if (item.children)
+	{
+		item.isOpen = false;
+		item.children.forEach((child) =>
+		{
+			child.isOpen = false;
+			if (child.children)
+			{
+				child.children.forEach((grandchild) =>
 				{
-					id: "1",
-					name: "Мягкая мебель",
-					isOpen: false,
-					children:
-					[
-						{
-							id: "1.1",
-							name: "Диваны",
-							isOpen: false,
-							children:
-							[
-								{
-									id: "1.1.1",
-									name: "В гостинную",
-								},
-							],
-						},
-					],
-				},
-			],
-		};
-	},
-	methods: {
-		openChildren(item)
-		{
-			item.isOpen = true;
-		},
-		closeChildren(item)
-		{
-			item.isOpen = false;
-		},
-	},
-};
+					grandchild.isOpen = false;
+				});
+			}
+		});
+	}
+}
 </script>
 <style lang="scss">
 @import "./styles/catalogMenuList";
