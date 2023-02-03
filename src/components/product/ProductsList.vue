24a4f4edff03dd7da1cc2c4c3a7ca59c64c718ae
Amiran <chicobes1@gmail.com>
2023-02-03 13:57:47 +0300

    created productList in pinia store

--- src/components/product/ProductsList.vue
+++ src/components/product/ProductsList.vue
@@ -1,7 +1,7 @@
 <template>
 	<ul class="products-list">
 		<ProductsItem
-			v-for="product in productItems"
+			v-for="product in productsStore.productList"
 	        :key="product.id"
 			:title="product.title"
 			:img="product.img"
@@ -11,14 +11,9 @@
 
 <script setup>
 import ProductsItem from "@/components/product/ProductsItem.vue";
-import { onMounted } from "vue";
 import { useProductStore } from "@/store/product/product";
 
-const productList = useProductStore()
-onMounted(() =>{
-	console.log("ProductsList mounted", productList);
-
-})
+const productsStore = useProductStore();
 </script>
 
 <style lang="scss">
--- src/store/product/product.js
+++ src/store/product/product.js
@@ -1,70 +1,73 @@
-import {defineStore} from "pinia";
+import { defineStore } from "pinia";
 
-export const useProductStore = defineStore({
-	state: () => ({
-		productList: [
-			{
-				id: 1,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 2,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 3,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 4,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 5,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 6,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 7,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 8,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 9,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 10,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 11,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-			{
-				id: 12,
-				title: "Красивый товар",
-				img: "@/assets/img/ProductImage-1.png",
-			},
-		],
-	}),
+export const useProductStore = defineStore('productId',{
+	state: () =>
+	{
+		return {
+			productList: [
+				{
+					id: 1,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 2,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 3,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 4,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 5,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 6,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 7,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 8,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 9,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 10,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 11,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+				{
+					id: 12,
+					title: "Красивый товар",
+					img: "@/assets/img/ProductImage-1.png",
+				},
+			],
+		};
+	},
 	getters:{
 		getProductList(state){
 			return state.productList;
