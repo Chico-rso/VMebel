355212d1cae919196e5271ffcefa910330d73c49
Amiran <chicobes1@gmail.com>
2023-03-15 12:34:50 +0300

    created detail-page__top blocks

--- src/views/detailPage/DetailPage.vue
+++ src/views/detailPage/DetailPage.vue
@@ -1,5 +1,9 @@
 <template>
 	<div class="detail-page">
+		<div class="detail-page__top">
+			<h3>Чайная машина Kitfort KT-630</h3>
+			<span>Артикул 100 024 215 307</span>
+		</div>
 		<div class="detail-page__slider">
 			<SliderCarousel />
 		</div>
